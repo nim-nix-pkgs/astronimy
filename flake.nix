43a10d9226d60f18cc5c40dd706e49297f7c0d11
hugosenari <hugosenari@gmail.com>
2022-05-12 11:49:45 -0300

    chore: re index astronimy

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."astronimy-master".dir   = "master";
+  inputs."astronimy-master".owner = "nim-nix-pkgs";
+  inputs."astronimy-master".ref   = "master";
+  inputs."astronimy-master".repo  = "astronimy";
+  inputs."astronimy-master".type  = "github";
+  inputs."astronimy-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."astronimy-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
